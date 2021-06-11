37e2cfa0364416d69f3faf1c50f36dc5cf9d6cf9
Nicholas Thompson (Jesse) <55792758+Nich-Thompson@users.noreply.github.com>
2021-06-11 16:17:22 +0200

    removed deprecated js file

--- goudendraak/resources/views/customer/category.blade.php
+++ goudendraak/resources/views/customer/category.blade.php
@@ -4,5 +4,3 @@
     <h1 class="display-3">{{ $category->name }}</h1><br>
     <category-menu :items="{{ $dishes }}"></category-menu>
 @endsection
-
-{{--<script src={{ asset('../js/orderItem.js') }} defer></script>--}}
