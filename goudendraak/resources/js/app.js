d9301b079c6980c1a02ebf87dddde866d2d60e7c
Nicholas Thompson (Jesse) <55792758+Nich-Thompson@users.noreply.github.com>
2021-06-15 15:08:25 +0200

    test

--- goudendraak/resources/js/app.js
+++ goudendraak/resources/js/app.js
@@ -4,17 +4,11 @@ import {createApp} from 'vue';
 import WebsiteHeader from "./components/website/header/WebsiteHeader";
 import ContentWrapper from "./components/website/content/ContentWrapper";
 import WebsiteContent from "./components/website/content/WebsiteContent";
-import Navbar from "./components/customer/Navbar";
-import MenuItem from "./components/customer/MenuItem";
-import CategoryMenu from "./components/customer/CategoryMenu";
 
 createApp({
     components: {
         WebsiteHeader,
         ContentWrapper,
         WebsiteContent,
-        Navbar,
-        MenuItem,
-        CategoryMenu,
     }
 }).mount('#app');
