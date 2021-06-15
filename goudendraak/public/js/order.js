0712cebfda4757051844631a5dc62e7efd741a8c
Nicholas Thompson (Jesse) <55792758+Nich-Thompson@users.noreply.github.com>
2021-06-15 15:26:41 +0200

    changed empty order view

--- goudendraak/public/js/order.js
+++ goudendraak/public/js/order.js
@@ -1,83 +1,73 @@
 window.onload = function () {
     // Load items currently on order
     let list = document.getElementById('itemList')
-    if (sessionStorage['order'] !== undefined) {
-        let items = JSON.parse(sessionStorage['order'])
+    if (sessionStorage['order'] === undefined) {
+        sessionStorage['order'] = JSON.stringify([])
+    }
+    let items = JSON.parse(sessionStorage['order'])
 
-        let totalPrice = 0
-        items.forEach(item => {
-            let newDiv = document.createElement('div')
-            newDiv.className = 'container p-1'
-            newDiv.id = item.id
-
-            let newItemSpan = document.createElement('span')
-            newItemSpan.className = 'dish'
-            newItemSpan.textContent = item.name
-
-            let newPriceSpan = document.createElement('span')
-            newPriceSpan.className = 'price'
-            newPriceSpan.textContent = '€ ' + item.price
-            totalPrice += parseFloat(item.price.replace(',', '.'))
-
-            let deleteButton = document.createElement('button')
-            deleteButton.className = 'btn btn-danger'
-            deleteButton.textContent = 'X'
-            deleteButton.addEventListener('click', function () {
-                newDiv.parentNode.removeChild(newDiv)
-                let order = sessionStorage['order']
-                let totalOrder = []
-                if (order !== undefined) {
-                    totalOrder = JSON.parse(sessionStorage['order'])
-                }
-                let index = totalOrder.map(function(e) { return e.id; }).indexOf(item.id);
-                totalOrder.splice(index, 1)
-                sessionStorage['order'] = JSON.stringify(totalOrder)
-
-                setValue()
-                setTotalPrice()
-            })
-
-            list.append(newDiv)
-            newDiv.append(deleteButton, newItemSpan, newPriceSpan)
-        })
-
-        // Add zeroes to totalPrice if necessary
-        totalPrice = totalPrice.toFixed(2)
-
-        let newDivider = document.createElement('hr')
-
-        let newPriceTextSpan = document.createElement('span')
-        newPriceTextSpan.className = 'dish'
-        newPriceTextSpan.textContent = 'Totaalprijs:'
-
-        let newTotalPriceSpan = document.createElement('span')
-        newTotalPriceSpan.id = 'totalPrice'
-        newTotalPriceSpan.className = 'price'
-        newTotalPriceSpan.textContent = '€ ' + totalPrice
-
-        list.append(newDivider, newPriceTextSpan, newTotalPriceSpan)
-
-        // Add method to button to clear order
-        let finalizeButton = document.getElementById('finalizeButton')
-        finalizeButton.addEventListener('click', function () {
-            sessionStorage.removeItem('order')
-        })
-
-        setValue()
-    } else {
+    let totalPrice = 0
+    items.forEach(item => {
         let newDiv = document.createElement('div')
         newDiv.className = 'container p-1'
+        newDiv.id = item.id
 
-        let newSpan = document.createElement('span')
-        newSpan.className = 'dish'
-        newSpan.textContent = 'U heeft nog niks op uw bestelling staan.'
+        let newItemSpan = document.createElement('span')
+        newItemSpan.className = 'dish'
+        newItemSpan.textContent = item.name
+
+        let newPriceSpan = document.createElement('span')
+        newPriceSpan.className = 'price'
+        newPriceSpan.textContent = '€ ' + item.price
+        totalPrice += parseFloat(item.price.replace(',', '.'))
+
+        let deleteButton = document.createElement('button')
+        deleteButton.className = 'btn btn-danger'
+        deleteButton.textContent = 'X'
+        deleteButton.addEventListener('click', function () {
+            newDiv.parentNode.removeChild(newDiv)
+            let order = sessionStorage['order']
+            let totalOrder = []
+            if (order !== undefined) {
+                totalOrder = JSON.parse(sessionStorage['order'])
+            }
+            let index = totalOrder.map(function (e) {
+                return e.id;
+            }).indexOf(item.id);
+            totalOrder.splice(index, 1)
+            sessionStorage['order'] = JSON.stringify(totalOrder)
+
+            setValue()
+            setTotalPrice()
+        })
 
         list.append(newDiv)
-        newDiv.append(newSpan)
+        newDiv.append(deleteButton, newItemSpan, newPriceSpan)
+    })
 
-        let finalizeButton = document.getElementById('finalizeButton')
-        finalizeButton.hidden = true
-    }
+    // Add zeroes to totalPrice if necessary
+    totalPrice = totalPrice.toFixed(2)
+
+    let newDivider = document.createElement('hr')
+
+    let newPriceTextSpan = document.createElement('span')
+    newPriceTextSpan.className = 'dish'
+    newPriceTextSpan.textContent = 'Totaalprijs:'
+
+    let newTotalPriceSpan = document.createElement('span')
+    newTotalPriceSpan.id = 'totalPrice'
+    newTotalPriceSpan.className = 'price'
+    newTotalPriceSpan.textContent = '€ ' + totalPrice
+
+    list.append(newDivider, newPriceTextSpan, newTotalPriceSpan)
+
+    // Add method to button to clear order
+    let finalizeButton = document.getElementById('finalizeButton')
+    finalizeButton.addEventListener('click', function () {
+        sessionStorage.removeItem('order')
+    })
+
+    setValue()
 }
 
 function setValue() {
