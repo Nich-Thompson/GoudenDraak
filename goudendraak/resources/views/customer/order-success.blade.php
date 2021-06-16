0164d315a45b8fb5fed19c29a481062358792de4
Wesley Salimans <wlt.salimans@student.avans.nl>
2021-06-16 13:23:05 +0200

    Create order-success.blade.php

---
+++ goudendraak/resources/views/customer/order-success.blade.php
@@ -0,0 +1,11 @@
+@extends('customer.layout')
+
+@section('content')
+    <h1 class="display-3">
+        Bestelling geplaatst!
+    </h1>
+    <p>
+        Onze koks gaan meteen aan de slag met jouw bestelling. Kijk ondertussen even naar onze cocktails.
+    </p>
+@endsection
+
