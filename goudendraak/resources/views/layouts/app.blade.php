5fb70033339e08cf7e398cce489a2d429418704a
Nicholas Thompson (Jesse) <55792758+Nich-Thompson@users.noreply.github.com>
2021-06-16 14:42:38 +0200

    Update app.blade.php

--- goudendraak/resources/views/layouts/app.blade.php
+++ goudendraak/resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
             <a class="navbar-brand" href="{{ route('getRegisterIndex') }}">
-                {{ config('app.name', 'Laravel') }}
+                Kassa
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -48,6 +48,7 @@
             @endguest
 
             <!-- Right Side Of Navbar -->
+                <ul></ul>
                 <ul class=" navbar-nav ml-auto">
                     <!-- Authentication Links -->
                     @guest
