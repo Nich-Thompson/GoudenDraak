604711f4d95d6a33f8df4c3eace96f64b2d9e7cd
Wesley Salimans <wlt.salimans@student.avans.nl>
2021-06-17 17:34:10 +0200

    update url

--- goudendraak/resources/views/menu.blade.php
+++ goudendraak/resources/views/menu.blade.php
@@ -2,7 +2,7 @@
 
 @section("content")
     <website-content>
-        <p>Download een PDF van het menu <a href="./download">hier</a>.</p>
+        <p>Download een PDF van het menu <a href="/menu/download" class="menu-download">hier</a>.</p>
         <img :src="'/img/restaurant-menukaart-1-2.jpg'" class="menu-img"/>
         <img :src="'/img/restaurant-menukaart-1.jpg'" class="menu-img"/>
     </website-content>
