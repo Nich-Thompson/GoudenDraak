2790bfc9b5c19cace73ddb48a55f587776fa26bb
Wesley Salimans <wlt.salimans@student.avans.nl>
2021-06-19 11:52:04 +0200

    Database models

--- goudendraak/database/seeders/DatabaseSeeder.php
+++ goudendraak/database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             DishSeeder::class,
             AllergySeeder::class,
             OldSalesSeeder::class,
+            NewsSeeder::class,
         ]);
     }
 }
