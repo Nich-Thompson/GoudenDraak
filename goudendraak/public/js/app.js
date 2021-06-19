b9a69d7e991f1b2f99891fade260eda45968fef3
Nicholas Thompson (Jesse) <55792758+Nich-Thompson@users.noreply.github.com>
2021-06-19 11:50:26 +0200

    updated packages

--- goudendraak/public/js/app.js
+++ goudendraak/public/js/app.js
@@ -17617,15 +17617,48 @@ __webpack_require__.r(__webpack_exports__);
     Array.from(document.getElementsByClassName('favorite-btn')).forEach(function (e) {
       e.addEventListener('click', function () {
         if (!e.classList.contains('favorited')) {
+          var createCookie = function createCookie(name, value, days) {
+            var expires;
+
+            if (days) {
+              var date = new Date();
+              date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+              expires = "; expires=" + date.toGMTString();
+            } else {
+              expires = "";
+            }
+
+            document.cookie = name + "=" + value + expires + "; path=/";
+          };
+
+          var getCookie = function getCookie(c_name) {
+            if (document.cookie.length > 0) {
+              var c_start = document.cookie.indexOf(c_name + "=");
+
+              if (c_start != -1) {
+                c_start = c_start + c_name.length + 1;
+                var c_end = document.cookie.indexOf(";", c_start);
+
+                if (c_end == -1) {
+                  c_end = document.cookie.length;
+                }
+
+                return unescape(document.cookie.substring(c_start, c_end));
+              }
+            }
+
+            return "";
+          };
+
           e.classList.add('favorited');
           var items = [];
 
-          if (localStorage['favorites'] !== undefined) {
-            items = JSON.parse(localStorage['favorites']);
+          if (getCookie('favorites') !== undefined) {
+            items = JSON.parse(getCookie('favorites'));
           }
 
           items.push(JSON.parse(e.value));
-          localStorage['favorites'] = JSON.stringify(items);
+          createCookie('favorites', JSON.stringify(items), 10);
         }
       });
     });
